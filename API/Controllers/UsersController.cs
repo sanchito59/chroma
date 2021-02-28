641ad862fc67ebb8a8e479988d0afc8f32669a12
Chris Sanchez <c.sanch7@gmail.com>
2021-02-28 12:05:05 -0800

    add functionality to GetUser and comment [Authorize] protection

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,12 +3,12 @@ using System.Threading.Tasks;
 using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-  // [Authorize]
+  // [Authorize] TODO: Assess if this should be protected
   public class UsersController : BaseApiController
   {
     private readonly IUnitOfWork _unitOfWork;
@@ -26,6 +26,13 @@ namespace API.Controllers
       var users = await _unitOfWork.UserRepository.GetUsersAsync();
 
       return Ok(users);
+    }
+
+    // GET user - api/users/:id
+    [HttpGet("{username}", Name = "GetUser")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
+      return await _unitOfWork.UserRepository.GetMemberAsync(username);
     }
   }
 }
