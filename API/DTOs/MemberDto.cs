013fd2d0a5055d2a8ec96988e786aa4d9ef093c5
Chris Sanchez <c.sanch7@gmail.com>
2021-02-27 17:51:55 -0800

    initial commit with folder structure for backend, DB setup, user model setup, auth setup, and basic middleware and error handling setup

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace API.DTOs
+{
+  public class MemberDto
+  {
+    public int Id { get; set; }
+    public string Username { get; set; }
+    // public string PhotoUrl { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+
+    // public ICollection<PaletteDto> Palettes { get; set; }
+  }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Backend aPI
+
+* C#
+* Entity Framework
+* PostgreSQL
+* Docker image for local DB
+
+Frontend Client (TODO)
+
+* Angular
+* Material UI
