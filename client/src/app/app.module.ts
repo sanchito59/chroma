d76e99d7b5d51574404d7d38a655d38eede97264
Chris Sanchez <c.sanch7@gmail.com>
2021-03-28 11:22:12 -0700

    add new-palette-color component with tooltip and copy on click functionality

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+// MATERIAL
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu'; 
 import { MatIconModule } from '@angular/material/icon'; 
@@ -14,13 +15,17 @@ import { MatButtonModule  } from '@angular/material/button';
 import { MatFormFieldModule  } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+// COMPONENTS
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { TextInputComponent } from './text-input/text-input.component';
 import { PaletteCardComponent } from './palette-card/palette-card.component';
 import { PaletteDetailComponent } from './palette-detail/palette-detail.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { NewPaletteColorComponent } from './new-palette-color/new-palette-color.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +35,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     TextInputComponent,
     PaletteCardComponent,
     PaletteDetailComponent,
-    HomepageComponent
+    HomepageComponent,
+    NewPaletteColorComponent
   ],
   imports: [
     BrowserModule,
@@ -47,6 +53,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     MatFormFieldModule,
     MatGridListModule,
     MatInputModule,
+    MatTooltipModule,
+    ClipboardModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
---
+++ client/src/app/new-palette-color/new-palette-color.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-palette-color',
+  templateUrl: './new-palette-color.component.html',
+  styleUrls: ['./new-palette-color.component.css']
+})
+export class NewPaletteColorComponent implements OnInit {
+  @Input() color: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.color)
+  }
+
+}
