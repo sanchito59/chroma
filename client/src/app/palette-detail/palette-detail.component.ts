9298be0ae5a29e06cff8b8b8786fff6f6c63d2cf
Chris Sanchez <c.sanch7@gmail.com>
2021-04-25 17:21:35 -0700

    add momentjs and style detail view

--- client/src/app/palette-detail/palette-detail.component.ts
+++ client/src/app/palette-detail/palette-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Palette } from '../_models/Palette';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-palette-detail',
@@ -9,12 +10,14 @@ import { Palette } from '../_models/Palette';
 })
 export class PaletteDetailComponent implements OnInit {
   palette: Palette;
+  formattedDate = moment().format('D/MM/YYYY');
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
       this.palette = data.PaletteDetailResolver;
+      this.formattedDate = moment(this.palette.created).format('MM/D/YYYY');
     });
   }
 }
